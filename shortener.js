918b90d1953cfb614ee956cdca851c8b8d125b3f
Mikita Manko <DjComandos@gmail.com>
2012-01-28 15:31:33 +0300

    - added more elements to test page - corrected behavior for multiple jQuery objects

--- shortener.js
+++ shortener.js
@@ -1,51 +1,53 @@
 (function($) {
 	$.fn.shorten = function(width, insertThreeDots) {
-		var self = $(this);
-		var ready = false;
-		var nodeNumber = 0;
-		var main = $('<div style="display: inline;position: absolute"></div>');
-		
-		main.append(self.html());
-		self.html(main);
-		
-		if(main.width() > width) {
-			if(insertThreeDots) {
-				main.append('<span class="threeDots">...</span>');
+		$.each(this, function (i, self) {
+			var self = $(self);
+			var ready = false;
+			var nodeNumber = 0;
+			var main = $('<div style="display: inline;position: absolute"></div>');
+			
+			main.append(self.html());
+			self.html(main);
+			
+			if(main.width() > width) {
+				if(insertThreeDots) {
+					main.append('<span class="threeDots">...</span>');
+				}
+				walkTroughDom(self[0]);
 			}
-			walkTroughDom(self[0]);
-		}
-		
-		self.html(main.html());
-		
-		function walkTroughDom(node) {
-			for( var i = node.childNodes.length - 1; i >= 0; i--){
-				if(!ready) {
-					var v = node.childNodes[i];
-					if(v.nodeType === 3) {
-						if(nodeNumber != 0 || !insertThreeDots) {
-							var cur = v.nodeValue;
-							v.nodeValue = "";
-							if(main.width() < width) {
-								var prev = "";
-								for(var j=0; j<cur.length; j++) {
-									prev = v.nodeValue;
-									v.nodeValue = cur.substring(0, j + 1);
-									if(main.width() > width) {
-										v.nodeValue = prev;
-										ready = true;
-										return;
+			
+			self.html(main.html());
+			
+			function walkTroughDom(node) {
+				for( var i = node.childNodes.length - 1; i >= 0; i--){
+					if(!ready) {
+						var v = node.childNodes[i];
+						if(v.nodeType === 3) {
+							if(nodeNumber != 0 || !insertThreeDots) {
+								var cur = v.nodeValue;
+								v.nodeValue = "";
+								if(main.width() < width) {
+									var prev = "";
+									for(var j=0; j<cur.length; j++) {
+										prev = v.nodeValue;
+										v.nodeValue = cur.substring(0, j + 1);
+										if(main.width() > width) {
+											v.nodeValue = prev;
+											ready = true;
+											return;
+										}
 									}
 								}
 							}
+						} else if(v.nodeType === 1){
+							walkTroughDom(v);
 						}
-					} else if(v.nodeType === 1){
-						walkTroughDom(v);
+						nodeNumber++;
+					} else {
+						return;
 					}
-					nodeNumber++;
-				} else {
-					return;
 				}
 			}
-		}
+		});
 	};
 })(jQuery);
