2a462526fa088d12cc1dd3a9353d184e6ef89ec0
Mikita Manko <DjComandos@gmail.com>
2012-01-28 15:56:28 +0300

    untabified

--- shortener.js
+++ shortener.js
@@ -1,53 +1,53 @@
 (function($) {
-	$.fn.shorten = function(width, insertThreeDots) {
-		$.each(this, function (i, self) {
-			var self = $(self),
-				ready = false,
-				nodeNumber = 0,
-				main = $('<div style="display: inline;position: absolute"></div>');
-			
-			function walkTroughDom(node) {
-				for( var i = node.childNodes.length - 1; i >= 0; i--){
-					if(!ready) {
-						var v = node.childNodes[i];
-						if(v.nodeType === 3) {
-							if(nodeNumber != 0 || !insertThreeDots) {
-								var cur = v.nodeValue;
-								v.nodeValue = "";
-								if(main.width() < width) {
-									var prev = "";
-									for(var j=0; j<cur.length; j++) {
-										prev = v.nodeValue;
-										v.nodeValue = cur.substring(0, j + 1);
-										if(main.width() > width) {
-											v.nodeValue = prev;
-											ready = true;
-											return;
-										}
-									}
-								}
-							}
-						} else if(v.nodeType === 1){
-							walkTroughDom(v);
-						}
-						nodeNumber++;
-					} else {
-						return;
-					}
-				}
-			}
-			
-			main.append(self.html());
-			self.html(main);
+    $.fn.shorten = function(width, insertThreeDots) {
+        $.each(this, function (i, self) {
+            var self = $(self),
+                ready = false,
+                nodeNumber = 0,
+                main = $('<div style="display: inline;position: absolute"></div>');
+            
+            function walkTroughDom(node) {
+                for( var i = node.childNodes.length - 1; i >= 0; i--){
+                    if(!ready) {
+                        var v = node.childNodes[i];
+                        if(v.nodeType === 3) {
+                            if(nodeNumber != 0 || !insertThreeDots) {
+                                var cur = v.nodeValue;
+                                v.nodeValue = "";
+                                if(main.width() < width) {
+                                    var prev = "";
+                                    for(var j=0; j<cur.length; j++) {
+                                        prev = v.nodeValue;
+                                        v.nodeValue = cur.substring(0, j + 1);
+                                        if(main.width() > width) {
+                                            v.nodeValue = prev;
+                                            ready = true;
+                                            return;
+                                        }
+                                    }
+                                }
+                            }
+                        } else if(v.nodeType === 1){
+                            walkTroughDom(v);
+                        }
+                        nodeNumber++;
+                    } else {
+                        return;
+                    }
+                }
+            }
+            
+            main.append(self.html());
+            self.html(main);
 
-			if(main.width() > width) {
-				if(insertThreeDots) {
-					main.append('<span class="threeDots">...</span>');
-				}
-				walkTroughDom(self[0]);
-			}
-			
-			self.html(main.html());
-		});
-	};
+            if(main.width() > width) {
+                if(insertThreeDots) {
+                    main.append('<span class="threeDots">...</span>');
+                }
+                walkTroughDom(self[0]);
+            }
+            
+            self.html(main.html());
+        });
+    };
 })(jQuery);
