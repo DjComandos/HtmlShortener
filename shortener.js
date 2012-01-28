a00b9636251bab8cc27d0805829be000799cf721
Mikita Manko <DjComandos@gmail.com>
2012-01-28 15:50:20 +0300

    refactored

--- shortener.js
+++ shortener.js
@@ -1,22 +1,10 @@
 (function($) {
 	$.fn.shorten = function(width, insertThreeDots) {
 		$.each(this, function (i, self) {
-			var self = $(self);
-			var ready = false;
-			var nodeNumber = 0;
-			var main = $('<div style="display: inline;position: absolute"></div>');
-			
-			main.append(self.html());
-			self.html(main);
-			
-			if(main.width() > width) {
-				if(insertThreeDots) {
-					main.append('<span class="threeDots">...</span>');
-				}
-				walkTroughDom(self[0]);
-			}
-			
-			self.html(main.html());
+			var self = $(self),
+				ready = false,
+				nodeNumber = 0,
+				main = $('<div style="display: inline;position: absolute"></div>');
 			
 			function walkTroughDom(node) {
 				for( var i = node.childNodes.length - 1; i >= 0; i--){
@@ -48,6 +36,18 @@
 					}
 				}
 			}
+			
+			main.append(self.html());
+			self.html(main);
+
+			if(main.width() > width) {
+				if(insertThreeDots) {
+					main.append('<span class="threeDots">...</span>');
+				}
+				walkTroughDom(self[0]);
+			}
+			
+			self.html(main.html());
 		});
 	};
 })(jQuery);
