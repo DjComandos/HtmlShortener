b9b66f03fea96310340c48aff3c7ed5c935273d9
Mikita Manko <DjComandos@gmail.com>
2012-03-19 21:45:59 +0300

    refactoring

--- shortener.js
+++ shortener.js
@@ -1,33 +1,41 @@
 (function($) {
     $.fn.shorten = function(width, insertThreeDots) {
-        $.each(this, function (i, self) {
+        $.each(this, function(i, self) {
             var self = $(self),
                 ready = false,
                 nodeNumber = 0,
-                main = $('<div class="trololo" style="display: inline !important;position: absolute !important"></div>');
-            
+                main = $('<div style="white-space: nowrap;display: inline; position: absolute"></div>');
+
+            function isInside() {
+                return main.width() < width;
+            }
+
+            function normalizeLastNode(cur, v) {
+                var prev = "";
+                for (var j = 0; j < cur.length; j++) {
+                    prev = v.nodeValue;
+                    v.nodeValue = cur.substring(0, j + 1);
+                    if (main.width() > width) {
+                        v.nodeValue = prev;
+                        ready = true;
+                        return;
+                    }
+                }
+            }
+
             function walkTroughDom(node) {
-                for( var i = node.childNodes.length - 1; i >= 0; i--){
-                    if(!ready) {
+                for (var i = node.childNodes.length - 1; i >= 0; i--) {
+                    if (!ready) {
                         var v = node.childNodes[i];
-                        if(v.nodeType === 3) {
-                            if(nodeNumber != 0 || !insertThreeDots) {
+                        if (v.nodeType === 3) {
+                            if (nodeNumber != 0 || !insertThreeDots) {
                                 var cur = v.nodeValue;
                                 v.nodeValue = "";
-                                if(main.width() < width) {
-                                    var prev = "";
-                                    for(var j=0; j<cur.length; j++) {
-                                        prev = v.nodeValue;
-                                        v.nodeValue = cur.substring(0, j + 1);
-                                        if(main.width() > width) {
-                                            v.nodeValue = prev;
-                                            ready = true;
-                                            return;
-                                        }
-                                    }
+                                if (isInside()) {
+                                    normalizeLastNode(cur, v);
                                 }
                             }
-                        } else if(v.nodeType === 1){
+                        } else if (v.nodeType === 1) {
                             walkTroughDom(v);
                         }
                         nodeNumber++;
@@ -36,18 +44,18 @@
                     }
                 }
             }
-            
+
             main.append(self.html());
             self.html(main);
 
-            if(main.width() > width) {
-                if(insertThreeDots) {
+            if (main.width() > width) {
+                if (insertThreeDots) {
                     main.append('<span class="threeDots">...</span>');
                 }
-                //walkTroughDom(self[0]);
+                walkTroughDom(self[0]);
             }
-            
-            //self.html(main.html());
+
+            self.html(main.html());
         });
     };
 })(jQuery);
